3fc8f167d340affda4665fae46000b34174841df
Vincent van Dijk <info@vincentvandijk.com>
2017-12-13 20:12:26 +0100

    Replaced method with trait.

--- src/Handler/AbstractCommandHandler.php
+++ src/Handler/AbstractCommandHandler.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 namespace ExtendsFramework\Command\Handler;
 
 use ExtendsFramework\Command\CommandMessageInterface;
-use ExtendsFramework\Command\Handler\Exception\MethodNotFound;
-use ReflectionMethod;
+use ExtendsFramework\Message\Payload\PayloadMethodTrait;
 
 abstract class AbstractCommandHandler implements CommandHandlerInterface
 {
+    use PayloadMethodTrait;
+
     /**
      * Method prefix.
      *
@@ -38,41 +39,7 @@ abstract class AbstractCommandHandler implements CommandHandlerInterface
         $this->aggregateId = $commandMessage->getAggregateId();
         $this->metaData = $commandMessage->getMetaData();
 
-        $method = $this->getMethod($commandMessage);
-
-        $method->invokeArgs($this, [$commandMessage->getPayload()]);
-    }
-
-    /**
-     * Get reflection method for event message payload.
-     *
-     * Method name is based on the method prefix combined with payload name.
-     *
-     * @param CommandMessageInterface $commandMessage
-     * @return ReflectionMethod
-     * @throws CommandHandlerException
-     */
-    protected function getMethod(CommandMessageInterface $commandMessage): ReflectionMethod
-    {
-        $method = $this->getPrefix() . $commandMessage
-                ->getPayloadType()
-                ->getName();
-
-        if (method_exists($this, $method) === false) {
-            throw new MethodNotFound($commandMessage);
-        }
-
-        return new ReflectionMethod($this, $method);
-    }
-
-    /**
-     * Get method prefix.
-     *
-     * @return string
-     */
-    protected function getPrefix(): string
-    {
-        return $this->prefix;
+        $this->getMethod($commandMessage)($commandMessage->getPayload());
     }
 
     /**
--- test/Handler/AbstractCommandHandlerTest.php
+++ test/Handler/AbstractCommandHandlerTest.php
@@ -57,50 +57,6 @@ class AbstractCommandHandlerTest extends TestCase
         $this->assertSame('foo', $listener->getAggregateId());
         $this->assertSame(['foo' => 'bar'], $listener->getMetaData());
     }
-
-    /**
-     * Method not found.
-     *
-     * Test that command handler can not be found and an exception will be thrown.
-     *
-     * @covers                   \ExtendsFramework\Command\Handler\AbstractCommandHandler::handle()
-     * @covers                   \ExtendsFramework\Command\Handler\AbstractCommandHandler::getMethod()
-     * @covers                   \ExtendsFramework\Command\Handler\Exception\MethodNotFound::__construct()
-     * @expectedException        \ExtendsFramework\Command\Handler\Exception\MethodNotFound
-     * @expectedExceptionMessage No command handler method found for payload name "PayloadFoo".
-     */
-    public function testMethodNotFound(): void
-    {
-        $payload = $this->createMock(PayloadInterface::class);
-
-        $payloadType = $this->createMock(PayloadTypeInterface::class);
-        $payloadType
-            ->method('getName')
-            ->willReturn('PayloadFoo');
-
-        $commandMessage = $this->createMock(CommandMessageInterface::class);
-        $commandMessage
-            ->method('getPayload')
-            ->willReturn($payload);
-
-        $commandMessage
-            ->method('getPayloadType')
-            ->willReturn($payloadType);
-
-        $commandMessage
-            ->method('getMetaData')
-            ->willReturn(['foo' => 'bar']);
-
-        $commandMessage
-            ->method('getAggregateId')
-            ->willReturn('foo');
-
-        /**
-         * @var CommandMessageInterface $commandMessage
-         */
-        $listener = new HandlerStub();
-        $listener->handle($commandMessage);
-    }
 }
 
 class HandlerStub extends AbstractCommandHandler
